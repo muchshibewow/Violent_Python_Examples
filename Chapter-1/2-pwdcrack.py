e0642cea2b06c1fc6101fb3d015c5eb215121a85
Pravin Pratap <pratappravin250@gmail.com>
2017-12-30 14:46:18 +0530

    Added code for Chapter 1

---
+++ Chapter-1/2-pwdcrack.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+# Written for the sole purpose of practice.
+# Examples taken (and some modified) from Violent Python.
+
+# This is the original example present in the book,
+# based on the UNIX crypt() function.
+
+from crypt import crypt
+import sys
+
+def testPass(passFile,dictFile):
+	"""
+		Checks if the hash in the passFile corresponds to 
+	    any of the words in the dictFile, using the salt 
+	    from the first 2 bytes of the hsah itself.
+	"""
+
+	# The password file contains just the single hashed password.
+	# The salt is the first 2 characters of the hash.
+	passwd = open(passFile,"r").readlines()[0]
+	salt = passwd[0:2]
+
+	# The dictionary file contains potential password candidates.
+	dictionary = open(dictFile,"r")
+
+	found = False
+	for word in dictionary.readlines():
+		
+		# The hash is "calculated" using the crypt() function,
+		# present in the crypt library. It takes the word and 
+		# the salt as arguments.
+		if crypt(word,salt) == passwd:
+			print ">> Password found : "+word
+			found = True
+			break
+	if not found:
+		# Password is not present in dictionary.
+		print ">> Password not found in the dictionary provided."
+
+def main():
+	if len(sys.argv) == 3:
+		testPass(sys.argv[1],sys.argv[2])
+	else:
+		print "Usage : python 2-pwdcrack.py <passwordfile> <dictionaryfile>"
+		exit(0)
+
+if __name__ == '__main__':
+	main()
